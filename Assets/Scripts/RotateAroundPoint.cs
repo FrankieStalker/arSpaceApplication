831d1f23cd5f3a931d43a8f8f8d24ba55812a17d
FrankieStalker <frankiestalker@gmail.com>
2022-01-21 23:13:53 +0000

    Touch controls
    Added touch controls using LenseTouch

--- Assets/Scripts/RotateAroundPoint.cs
+++ Assets/Scripts/RotateAroundPoint.cs
@@ -9,12 +9,6 @@ public class RotateAroundPoint : MonoBehaviour
     //Variable to hold object to rotate around
     public GameObject pivotObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
