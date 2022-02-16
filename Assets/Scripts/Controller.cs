a664a484bf1dd0197b34461a30fa868e98be4151
FrankieStalker <frankiestalker@gmail.com>
2022-02-16 18:25:18 +0000

    A lot of stuff
    - Home UI
    - Camera Interface
    - Multiple scenes

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject titleScreen;
+    public GameObject homePage;
+
+    public void Update()
+    {
+        if(titleScreen == true || homePage == true)
+        {
+            Screen.orientation = ScreenOrientation.Portrait;
+        }
+        else
+        {
+            Screen.orientation = ScreenOrientation.Landscape;
+        }
+    }
+    public void titleScreenProceed()
+    {
+        homePage.SetActive(true);
+        titleScreen.SetActive(false);
+    }
+    public void LoadLevel(string newLevel)
+    {
+        //When level load is clicked load selected level
+        //When level load is clicked make screen in landscape mode
+        Screen.orientation = ScreenOrientation.Landscape;
+        SceneManager.LoadScene(newLevel);
+    }
+
+    public void back(string back)
+    {
+        //When back is clicked load home UI scene
+        //When back is clicked make screen in portrait mode
+        Screen.orientation = ScreenOrientation.Portrait;
+        SceneManager.LoadScene(back);
+    }
+}
--- Assets/Scripts/RotateAroundPoint.cs
+++ Assets/Scripts/RotateAroundPoint.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class RotateAroundPoint : MonoBehaviour
 {
+
     //Variable to control the speed of rotation
     public float rotationSpeed;
     //Variable to hold object to rotate around
     public GameObject pivotObject;
+    bool rotateStatus = false;
+
+    //Rotate object function
+    public void RotateObject()
+    {
+        if(rotateStatus == false)
+        {
+            rotateStatus = true;
+        }
+        else
+        {
+            rotateStatus = false;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(pivotObject.transform.position, new Vector3(0, 1, 0), rotationSpeed * Time.deltaTime);
+        if(rotateStatus == true)
+        {
+            //Rotate object around point once rotate status = true
+            transform.RotateAround(pivotObject.transform.position, new Vector3(0, 1, 0), rotationSpeed * Time.deltaTime);
+        }
+        
     }
 }
