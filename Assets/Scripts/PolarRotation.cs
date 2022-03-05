44c5ebd8364eef120dd3a9f94ae1aec65a452ae4
FrankieStalker <frankiestalker@gmail.com>
2022-03-05 11:04:18 +0000

    final final commit

--- Assets/Scripts/PolarRotation.cs
+++ Assets/Scripts/PolarRotation.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateAroundPoint : MonoBehaviour
+public class PolarRotation : MonoBehaviour
 {
-
     //Variable to control the speed of rotation
     public float rotationSpeed;
     //Variable to hold object to rotate around
@@ -28,11 +27,11 @@ public class RotateAroundPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(rotateStatus == true)
+        if (rotateStatus == true)
         {
             //Rotate object around point once rotate status = true
-            transform.RotateAround(pivotObject.transform.position, new Vector3(0, 1, 0), rotationSpeed * Time.deltaTime);
+            transform.RotateAround(pivotObject.transform.position, new Vector3(0, 0, 1), rotationSpeed * Time.deltaTime);
         }
-        
+
     }
 }
